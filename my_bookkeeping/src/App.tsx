c26ee632801fd1ebb34cff25a162c01b7b8045ab
trias <triasp.451@gmail.com>
2025-03-16 14:40:15 +0700

    setup react and routing

---
+++ my_bookkeeping/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path='/'/>
+      <Route path='/pendapatan'/>
+      <Route path='/pengeluaran'/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
